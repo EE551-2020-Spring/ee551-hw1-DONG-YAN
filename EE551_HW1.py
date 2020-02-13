b0a905e9a6a2e4b23d18aabc6d84bec68d4d20c7
DONG-YAN <61001039+DONG-YAN@users.noreply.github.com>
2020-02-13 14:59:09 -0500

    Update EE551_HW1.py

--- EE551_HW1.py
+++ EE551_HW1.py
@@ -14,15 +14,17 @@ def numbers_and_strings():
     x = "EE551"
 
     # Assign a string "Stevens" to a variable y
-
+    y = "Stevens"
     # Repeat variable y 5 times
-
+    z = y*5
     # What is the length of z?
-
+    len(z)
     # Concatenate variable y with string " is good"
-
+    m = y+" is good"
+    print(m)
     # Replace "good" with "awesome" in variable m and assign it to a new variable n
-
+    n = m.replace("good","awesome")
+    print(n)
     return x, y, z, length, m, n
 
 
@@ -33,18 +35,34 @@ def lists():
     n = "Stevens is awesome"
 
     # Split variable n on a delimiter space into a list of substrings
-
+    p = n.split()
+    print(p)
     # Get all the items past the first of the third substring
-
+    r=p[2].replace("awesome","wesome")
+    print(r)
     # Create a 3 x 3 matrix as nested list such that
     #   first row is [1, 4, 5]
     #   second row is [6, 10, 11]
     #   third row is [12, 17, 38]
-
+    import numpy as np
+    C=np,matrix([[1,4,5],[6,10,11],[12,17,38]])
+    print(C)
     # Collect the items in the last column of matrix A using list comprehension
+    def last_column(C):
+    m = c.tolist()
+    lst = []
 
-    # Collect only the even items of the diagonal of matrix A using list comprehension
-
+    for i in range(len(c)):
+        lst.append(m[i][-1])
+    
+    return lst
+    last_column(C)
+    d=last_column(C)
+    # Collect only the even items of the diagonal of matrix C using list comprehension
+    o=[1,10,38,5,10,12]
+    o.remove[4]
+    print(o)
+    
     # We can convert a single character to its underlying integer code (e.g., its ASCII byte value)
     # by passing it to the built-in ord function. Generate a list of these integers to represent
     # each character of the string "Stevens" using list comprehension.
@@ -60,24 +78,30 @@ def dictionaries():
     #   fruit => "apple"
     #   quantity => 4
     #   color => "green"
-
+    f={'fruit':'apple','quantity':'4','color':'green'}
+    print(f)
     # Get the item in dictionary f that the key "fruit" maps to
-
+    f['fruit']
     # Increase the quantity of f by 1
     # IMPLEMENT IT HERE
-
+    f['quantity']=5
+    print(f)
     # Create a nested dictionary where:
     #   name => {first_name => "Grace", last_name => "Hopper"} (a dictionary)
     #   jobs => ["scientist", "engineer"] (a list)
     #   age => 85
-
+    name={'first_name':'Grace','last_name':'Hopper'}
+    jobs=["scientist","engineer"]
+    age="85"
+    a={"name","jobs","age"}
     # Add "programmer" to the list of jobs Grace has
     # IMPLEMENT IT HERE
-
+    jobs.append("programmer")
+    print(jobs)
     # Get the third job Grace has that you recently added
-
+    p= jobs[2]
     # Use the sort() function to get sorted keys of amazing_grace in alphabetically ascending order
-
+    k=
     return a, f, p, k
 
 numbers_and_strings()
